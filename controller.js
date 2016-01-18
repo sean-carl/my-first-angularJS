a8efcf243f3b3e976953b52cf6f266db201763e6
sean-carl <c02011carl@gmail.com>
2016-01-18 10:35:16 +0800

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,17 @@
+function TodoCrtlRemovable($scope) 
+{  
+  $scope.newItem = "";  
+  $scope.todoList = [{ label: "買牛奶" }, { label: "繳電話費" }];  
+  $scope.addItem = function()
+  {
+  	  if(this.newItem)
+  	  {
+  	  	this.todoList.push({label:this.newItem,isFinish:false});
+  	  	this.newItem = "";
+  	  }
+  }
+  $scope.removeItem = function(item)
+  {
+  	  item.isFinish = true;
+  }
+}  
