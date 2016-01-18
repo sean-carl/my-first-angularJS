60bd336fb7b1ba19944855a2ad7f88469b9e0e5d
sean-carl <c02011carl@gmail.com>
2016-01-18 11:07:49 +0800

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,17 +1,65 @@
-function TodoCrtlRemovable($scope) 
-{  
-  $scope.newItem = "";  
-  $scope.todoList = [{ label: "買牛奶" }, { label: "繳電話費" }];  
+function TodoCrtl($scope) 
+{
+  $scope.newItem = "";
+  $scope.todoList = [];
   $scope.addItem = function()
   {
-  	  if(this.newItem)
-  	  {
-  	  	this.todoList.push({label:this.newItem,isFinish:false});
-  	  	this.newItem = "";
-  	  }
+    if(this.newItem)
+    {
+       this.todoList.push({label:this.newItem});
+       this.newItem = "";
+    }
+   
   }
+} 
+    
+    
+function TodoCrtlRemovable($scope) 
+{
+  $scope.newItem = "";
+  $scope.todoList = [];
+  $scope.addItem = function()
+  {
+    if(this.newItem)
+    {
+       this.todoList.push({label:this.newItem,isFinish:false});
+       this.newItem = "";
+    }   
+  }   
+      
   $scope.removeItem = function(item)
   {
-  	  item.isFinish = true;
+      item.isFinish = true;
+  }       
+}     
+
+
+function TodoCrtlUpdate($scope) 
+{
+  $scope.newItem = "";
+  $scope.todoList = [];
+  $scope.addItem = function()
+  {
+    if(this.newItem)
+    {
+       this.todoList.push({label:this.newItem,isFinish:false});
+       this.newItem = "";
+    } 
   }
-}  
+
+  $scope.removeItem = function(item)
+  {
+      item.isFinish = true;
+  }
+
+
+  $scope.edit = function(item)
+  {
+      item.editing = true;
+  }
+
+  $scope.save = function(item)
+  {
+    delete item.editing;
+  }
+}
