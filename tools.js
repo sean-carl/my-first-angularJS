c5740122cd85c102a38e7652b00eb43dc7a211c1
Sean Huang <c02011carl@gmail.com>
2016-04-03 13:23:37 +0800

    flaticon.jpg

--- tools.js
+++ tools.js
@@ -228,7 +228,7 @@ app.controller('ToolsCtrl', function($scope) {
         "name": "flaticon",
         "intro": "最大的向量圖資料庫",
         "link": "http://www.flaticon.com/",
-        "url": "image/flaticon.png",
+        "url": "image/flaticon.jpg",
         "tag":["visualization", "icon"]
     }, {
         "name": "Chartico",
