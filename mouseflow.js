b4a6829585e4c9bf2fda6e065f73952d67bf0b44
Sean Huang <c02011carl@gmail.com>
2016-04-19 01:11:56 +0800

    mouseflow

---
+++ mouseflow.js
@@ -0,0 +1,7 @@
+var _mfq = _mfq || [];
+  (function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.async = true;
+    mf.src = "//cdn.mouseflow.com/projects/bf072b70-cf43-4619-968f-cd4e784f57a6.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+  })();
