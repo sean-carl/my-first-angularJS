7449f747ee7aa8eb0bafff787fefbc8630d0a965
Sean Huang <c02011carl@gmail.com>
2016-07-12 16:54:32 +0800

    utm_source tracke

--- tools.js
+++ tools.js
@@ -5,14 +5,30 @@ $( '#nav-colapse').click(function() {
 
 /* navbar */
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
 
 
 /*indexready*/
 
 $(function(){
     $(document).ready(function(){
-    amplitude.logEvent('index');
-    console.log("ready");
+        if (getUrlParameter('utm_source') === 'fb') {
+            amplitude.logEvent('fb');    
+            console.log("ready");
+        }
     });
 });
 
